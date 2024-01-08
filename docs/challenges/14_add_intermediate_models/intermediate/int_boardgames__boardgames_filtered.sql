7e77a37724d9e00d55d064d413c0e2bc139f92bd
filipebalseiro <filipe_balseiro@hakkoda.io>
2024-01-08 15:26:06 +0000

    feat: added challenge 14_add_intermediate_models

---
+++ docs/challenges/14_add_intermediate_models/intermediate/int_boardgames__boardgames_filtered.sql
@@ -0,0 +1,22 @@
+with
+    
+boardgames as (
+    select * from {{ ref('stg_boardgames__boardgames') }}
+),
+
+reviews as (
+    select * from {{ ref('stg_boardgames__reviews') }}
+),
+
+boardgames_filtered as (
+    select
+        *
+    from boardgames
+    where 
+        boardgame_id in (select boardgame_id from reviews)
+        and boardgame_type <> 'boardgame'
+        and boardgame_avg_rating <> -1
+        and boardgame_avg_weight <> -1
+)
+
+select * from boardgames_filtered
---
+++ docs/challenges/14_add_intermediate_models/intermediate/int_users_countries_joined.sql
@@ -0,0 +1,27 @@
+with
+    
+users as (
+    select * from {{ ref('stg_boardgames__users') }}
+),
+
+countries as (
+    select * from {{ ref('stg_country_codes') }}
+),
+
+countries_users_ref as (
+    select * from {{ ref('stg_country_codes_users_ref') }}
+),
+
+users_countries_joined as (
+    select
+        users.user_username,
+        users.user_url,
+        coalesce(countries.country_code, countries_users_ref.country_code) as country_code,
+        coalesce(countries.country_name, countries_users_ref.country_name) as country_name
+    from users
+    left join countries_users_ref on users.country_name = countries_users_ref.country_name
+    left join countries on countries.country_name = users.country_name
+
+)
+
+select * from users_countries_joined
